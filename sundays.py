9805171107de73b3d0c7aed1e545bceb4561168d
Junhyeok Lee <z5267156@unsw.edu.au>
2022-11-16 21:51:32 +1100

    changed function to spit out sundays of the given month using args

--- sundays.py
+++ sundays.py
@@ -1,18 +1,24 @@
 # importing the module
-import pandas as pd
-  
-# target year
-year = "2022"
-  
-# instantiating the parameters
-start = year + "-01-01"
-periods = 52
-freq = "W-SUN"
-  
-# fetching the Sundays
-sundays = pd.date_range(start = start,
-                        periods = periods,
-                        freq = freq)
-  
-# printing the Sundays                        
-print(sundays)
+
+
+import datetime
+import sys
+import calendar
+
+def sundays(month):
+    # target year
+    # month = sys.argv[1:][0]
+    year = "2022"
+    
+    # instantiating the parameters
+    start = datetime.date(int(year), month, 1)
+    i = 1
+    list = []
+    while i < calendar.monthrange(int(year), month)[1]:
+        if datetime.date(int(year), month, i).isoweekday() == 7:
+            list.append(datetime.date(int(year), month, i).isoformat())
+        i += 1
+    return list
+
+# argc = len(sys.argv)      <- remove comment to use the program as a stand alone function
+# sundays(int(sys.argv[1:][0]))
